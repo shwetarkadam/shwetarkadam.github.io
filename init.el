5707cf7ee8378eb457806e2308b6e160e3f3b4e4
Shweta Kadam <shwetarkadam25@gmail.com>
2022-05-07 08:26:57 +0530

    Update init.el

--- init.el
+++ init.el
@@ -50,13 +50,15 @@
         (error (format "%s is not set or is not an existing directory (%s)" env-key env-value)))))
 
   (setq org-hugo-section "notes")
-(setq org-id-extra-files (directory-files-recursively  notes-org-files "\.org$"))
+
+  
+  (setq org-id-extra-files (directory-files-recursively  notes-org-files "\.org$"))
+  (setf org-id-extra-files (directory-files-recursively  notes-org-files "\.org$"))
 ;;   (let ((org-id-extra-files  notes-org-files "\.org$"))
 ;; (org-hugo-export-wim-to-md))
   
   
   (dolist (org-file (directory-files-recursively notes-org-files "\.org$"))
-    (org-id-extra-files (directory-files-recursively notes-org-files "\.org$"))
     (with-current-buffer (find-file org-file)
       (message (format "[build] Exporting %s" org-file))
       (org-hugo-export-wim-to-md :all-subtrees nil nil nil)))
